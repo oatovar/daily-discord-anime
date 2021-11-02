8ac786a9c00cc03367acff9e4da5d862594ddc1b
Oscar Tovar <33381041+oatovar@users.noreply.github.com>
2021-11-02 01:19:44 -0400

    Fix deno deploy (#2)

--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
 import { app } from "./src/app.ts";
 
-await app.listen("localhost:8080");
+const listener = Deno.listen({ hostname: "localhost", port: 8080 });
+
+for await (const conn of listener) {
+  (async () => {
+    const requests = Deno.serveHttp(conn);
+    for await (const { request, respondWith } of requests) {
+      const response = await app.handle(request, conn);
+      if (response) {
+        respondWith(response);
+      }
+    }
+  });
+}
