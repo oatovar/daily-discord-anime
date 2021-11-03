72a8a69fc4fca24b5e5920f424d9f903698be129
Oscar A. Tovar <oscar.alberto.tovar@gmail.com>
2021-11-02 22:03:47 -0400

    Add middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import {
   Application,
   Context,
+  etag,
   Router,
 } from "https://deno.land/x/oak@v9.0.1/mod.ts";
+import logger from "https://deno.land/x/oak_logger@1.0.0/mod.ts";
 import {
   CallbackType,
   InteractionResponse,
@@ -13,8 +15,32 @@ import { verifier } from "./verifier.ts";
 
 export const app = new Application();
 
+// Add basic logging
+app.use(logger.logger);
+// Add response time logging.
+app.use(logger.responseTime);
+// Add etag support to reduce caching.
+app.use(etag.factory());
+// Handle errors gracefully.
+app.use(async (ctx: Context, next: () => Promise<unknown>) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.response.status = 500;
+    ctx.response.body = { msg: err.message };
+  }
+});
+
+// Add discord commands
 const commands = new Router();
+// Add healthz endpoint before verification
+commands.all("/healthz", (ctx: Context) => {
+  ctx.response.body = "OK";
+});
+// Add verification for all discord webhook reqs
 commands.use(verifier);
+// TODO(oatovar): refactor this to use a slash command
+// router.
 commands.all("/", async (ctx: Context) => {
   const interaction = JSON.parse(
     ctx.response.body?.toString() ?? "",
@@ -40,6 +66,7 @@ commands.all("/", async (ctx: Context) => {
   ctx.throw(400);
 });
 
+// Add routes
 app.use(
   commands.routes(),
   commands.allowedMethods(),
